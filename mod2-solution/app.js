3e6c2c0e9d5e4024632d6d2786522dc996ffac25
Neha <nehajagtap27@gmail.com>
2020-05-22 13:24:24 +0530

    new

---
+++ mod2-solution/app.js
@@ -0,0 +1,105 @@
+(function(){
+'use strict';
+
+angular.module('ShoppingListCheckOff',[])
+.controller('ToBuyController',ToBuyController)
+.controller('AlreadyBoughtController',AlreadyBoughtController)
+.service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService)
+{
+    var list1 = this;
+
+    list1.itemName = "";
+    list1.itemQuantity = "";
+
+    list1.itemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+
+    list1.AddToBought = function ()
+    {
+      try
+      {
+        ShoppingListCheckOffService.AddToBought(list1.itemName,list1.itemQuantity);
+      }
+      catch(error)
+      {
+         list1.errorMessage = error.message;
+      }
+    }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService)
+{
+  var list2 = this;
+
+  try
+  {
+  list2.itemsBought = ShoppingListCheckOffService.getItemsBought();
+  }
+  catch(error)
+  {
+    list2.errorMessage = error.message;
+  }
+}
+
+
+function ShoppingListCheckOffService()
+{
+  var service = this;
+
+  var itemsToBuy = [
+    {
+      name:"cookies",
+      quantity:"10 bags"
+    },
+    {
+      name:"chips",
+      quantity:"20 bags"
+    },
+    {
+      name:"milk",
+      quantity:"10 bottles"
+    }
+  ];
+  var itemsBought = [];
+
+  service.AddToBought = function (itemName,itemQuantity,itemIndex)
+  {
+    var items = {
+      name : itemName,
+      quantity : itemQuantity
+    };
+    if(itemsToBuy.length != 0)
+    {
+      items.push(itemsBought);
+      items.splice(itemIndex,1)
+    }
+   else {
+      throw new Error("Everything is bought!");
+   }
+  };
+
+  service.getItemsToBuy = function()
+  {
+      return itemsToBuy;
+  };
+
+  service.getItemsBought = function()
+  {
+    if(itemsBought.length != 0)
+    {
+        return itemsBought;
+    }
+    else
+    {
+      throw new Error("Nothing bought yet.");
+    }
+
+  };
+
+}
+
+
+})();
